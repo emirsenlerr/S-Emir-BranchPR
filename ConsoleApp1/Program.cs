b79eaf4dddb04b42b7f0ed4f7683634c55b4bf91
Emir Şenler <emir.senler@sekompos.com>
2024-08-27 13:24:40 +0300

    I taked age value from user.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,6 +2,9 @@
 
 Console.WriteLine("İsminizi Giriniz: ");
 string name = Console.ReadLine();
- 
-Person p = new Person(19, name);
+
+Console.WriteLine("Yaşınızı Giriniz: ");
+int yas = int.Parse(Console.ReadLine());
+
+Person p = new Person(yas, name);
 
